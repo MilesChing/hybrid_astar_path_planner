7246622a889685b2657ed42ccdd30c4aa27b6f25
MilesChing <qinzhengrui@live.cn>
2020-03-14 14:57:12 +0800

    Initialize

---
+++ build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/milesching/workspace/TiEV/exps/src/hybrid_atar_path_planner/src/algorithm.cpp" "/home/milesching/workspace/TiEV/exps/src/hybrid_atar_path_planner/build/CMakeFiles/test.dir/src/algorithm.cpp.o"
+  "/home/milesching/workspace/TiEV/exps/src/hybrid_atar_path_planner/src/collisiondetection.cpp" "/home/milesching/workspace/TiEV/exps/src/hybrid_atar_path_planner/build/CMakeFiles/test.dir/src/collisiondetection.cpp.o"
+  "/home/milesching/workspace/TiEV/exps/src/hybrid_atar_path_planner/src/dubins.cpp" "/home/milesching/workspace/TiEV/exps/src/hybrid_atar_path_planner/build/CMakeFiles/test.dir/src/dubins.cpp.o"
+  "/home/milesching/workspace/TiEV/exps/src/hybrid_atar_path_planner/src/main.cpp" "/home/milesching/workspace/TiEV/exps/src/hybrid_atar_path_planner/build/CMakeFiles/test.dir/src/main.cpp.o"
+  "/home/milesching/workspace/TiEV/exps/src/hybrid_atar_path_planner/src/node2d.cpp" "/home/milesching/workspace/TiEV/exps/src/hybrid_atar_path_planner/build/CMakeFiles/test.dir/src/node2d.cpp.o"
+  "/home/milesching/workspace/TiEV/exps/src/hybrid_atar_path_planner/src/node3d.cpp" "/home/milesching/workspace/TiEV/exps/src/hybrid_atar_path_planner/build/CMakeFiles/test.dir/src/node3d.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "algorithm.h"
+#include <iostream>
+using namespace std;
+using namespace HybridAStar;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
